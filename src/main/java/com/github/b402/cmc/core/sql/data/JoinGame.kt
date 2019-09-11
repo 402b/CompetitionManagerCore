c71f99fe5dc702a0e05fda161f7c525d8d182d2a
279979091 <279979091@qq.com>
2019-09-11 21:27:04 +0800

    修正了game_Join接口错误

--- src/main/java/com/github/b402/cmc/core/sql/data/JoinGame.kt
+++ src/main/java/com/github/b402/cmc/core/sql/data/JoinGame.kt
@@ -4,7 +4,7 @@ import com.github.b402.cmc.core.sql.SQLManager
 
 object JoinGame {
     suspend fun joinGame(uid: Int, gid: Int) = SQLManager.async {
-        val ps = this.prepareStatement("INSERT INTO JoinGame VALUES (?,?,?)")
+        val ps = this.prepareStatement("INSERT INTO JoinGame （UID,GID,Time) VALUES (?,?,?)")
         ps.setInt(1, uid)
         ps.setInt(2, gid)
         ps.setLong(3, System.currentTimeMillis())
